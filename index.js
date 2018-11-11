5e634964bcb261d6aa23724e4dfb1a8e177661a8
john muhl <git@johnmuhl.com>
2018-11-11 13:55:13 -0600

    add some prose

--- index.js
+++ index.js
@@ -8,15 +8,24 @@ export default [
 			{
 				name: "American Light Lager",
 				id: "1A",
-				overall_impression: "",
-				aroma: "",
-				appearance: "",
-				flavor: "",
-				mouthfeel: "",
-				comments: "",
-				history: "",
-				characteristic_ingredients: "",
-				style_comparison: "",
+				overall_impression:
+					"Highly carbonated, very light-bodied, nearly flavorless lager designed to be consumed very cold. Very refreshing and thirst quenching.",
+				aroma:
+					"Low to no malt aroma, although it can be perceived as grainy, sweet, or corn-like if present. Hop aroma is light to none, with a spicy or floral hop character if present. While a clean fermentation character is desirable, a light amount of yeast character (particularly a light apple fruitiness) is not a fault. Light DMS is not a fault.",
+				appearance:
+					"Very pale straw to pale yellow color. White, frothy head seldom persists. Very clear.",
+				flavor:
+					"Relatively neutral palate with a crisp and dry finish and a low to very low grainy or corn-like flavor that might be perceived as sweetness due to the low bitterness. Hop flavor ranges from none to low levels, and can have a floral, spicy, or herbal quality (although rarely strong enough to detect). Low to very low hop bitterness. Balance may vary from slightly malty to slightly bitter, but is relatively close to even. High levels of carbonation may accentuate the crispness of the dry finish. Clean lager fermentation character.",
+				mouthfeel:
+					"Very light (sometimes watery) body. Very highly carbonated with slight carbonic bite on the tongue.",
+				comments:
+					"Designed to appeal to as broad a range of the general public as possible. Strong flavors are a fault.",
+				history:
+					"Coors briefly made a light lager in the early 1940s. Modern versions were first produced by Rheingold in 1967 to appeal to diet-conscious drinkers, but only became popular starting in 1973 after Miller Brewing acquired the recipe and marketed the beer heavily to sports fans with the “tastes great, less filling” campaign. Beers of this genre became the largest sellers in the United States in the 1990s.",
+				characteristic_ingredients:
+					"Two- or six-row barley with high percentage (up to 40%) of rice or corn as adjuncts. Additional enzymes can further lighten the body and lower carbohydrates.",
+				style_comparison:
+					"A lighter-bodied, lower-alcohol, lower calorie version of an American Lager. Less hop character and bitterness than a Leichtbier.",
 				vital_statistics: {
 					original_extract: { min: 7, max: 10 },
 					terminal_extract: { min: -0.5, max: 2 },
@@ -46,15 +55,24 @@ export default [
 			{
 				name: "American Lager",
 				id: "1B",
-				overall_impression: "",
-				aroma: "",
-				appearance: "",
-				flavor: "",
-				mouthfeel: "",
-				comments: "",
-				history: "",
-				characteristic_ingredients: "",
-				style_comparison: "",
+				overall_impression:
+					"A very pale, highly-carbonated, light-bodied, well-attenuated lager with a very neutral flavor profile and low bitterness. Served very cold, it can be a very refreshing and thirst quenching drink.",
+				aroma:
+					"Low to no malt aroma, although it can be perceived as grainy, sweet or corn-like if present. Hop aroma may range from none to a light, spicy or floral hop presence. While a clean fermentation character is desirable, a light amount of yeast character (particularly a light apple character) is not a fault. Light DMS is also not a fault.",
+				appearance:
+					"Very pale straw to medium yellow color. White, frothy head seldom persists. Very clear.",
+				flavor:
+					"Relatively neutral palate with a crisp and dry finish and a moderately-low to low grainy or corn-like flavor that might be perceived as sweetness due to the low bitterness. Hop flavor ranges from none to moderately-low levels, and can have a floral, spicy, or herbal quality (although often not strong enough to distinguish). Hop bitterness at low to medium-low level. Balance may vary from slightly malty to slightly bitter, but is relatively close to even. High levels of carbonation may accentuate the crispness of the dry finish. Clean lager fermentation character.",
+				mouthfeel:
+					"Low to medium-low body. Very highly carbonated with slight carbonic bite on the tongue.",
+				comments:
+					"Strong flavors are a fault. Often what non-craft beer drinkers expect to be served if they order beer in the United States. May be marketed as Pilsner beers outside of Europe, but should not be confused with traditional examples.",
+				history:
+					"Although German immigrants had brewed traditional Pilsner-inspired lager beer in the United States since the mid-late 1800s, the modern American lager style was heavily influenced by Prohibition and World War II. Surviving breweries consolidated, expanded distribution, and heavily promoted a beer style that was appealing to a broad range of the population. Became the dominant beer style for many decades, and spawning many international rivals who would develop similarly bland products for the mass market supported by heavy advertising.",
+				characteristics_ingredients:
+					"Two- or six-row barley with high percentage (up to 40%) of rice or corn as adjuncts.",
+				style_comparison:
+					"Stronger, more flavor and body than a Light American Lager. Less bitterness and flavor than an International Lager. Significantly less flavor, hops, and bitterness than traditional European Pilsners.",
 				vital_statistics: {
 					original_extract: { min: 10, max: 12.3 },
 					terminal_extract: { min: 1, max: 2.6 },
@@ -84,15 +102,24 @@ export default [
 			{
 				name: "Cream Ale",
 				id: "1C",
-				overall_impression: "",
-				aroma: "",
-				appearance: "",
-				flavor: "",
-				mouthfeel: "",
-				comments: "",
-				history: "",
-				characteristic_ingredients: "",
-				style_comparison: "",
+				overall_impression:
+					"A clean, well-attenuated, flavorful American “lawnmower” beer. Easily drinkable and refreshing, with more character than typical American lagers.",
+				aroma:
+					"Medium-low to low malt notes, with a sweet, corn-like aroma. Low levels of DMS are allowable, but are not required. Hop aroma medium low to none, and can be of any variety although floral, spicy, or herbal notes are most common. Overall, a subtle aroma with neither hops nor malt dominating. Low fruity esters are optional.",
+				appearance:
+					"Pale straw to moderate gold color, although usually on the pale side. Low to medium head with medium to high carbonation. Fair head retention. Brilliant, sparkling clarity.",
+				flavor:
+					"Low to medium-low hop bitterness. Low to moderate maltiness and sweetness, varying with gravity and attenuation. Usually well-attenuated. Neither malt nor hops dominate the palate. A low to moderate corny flavor is commonly found, as is light DMS (optional). Finish can vary from somewhat dry to faintly sweet. Low fruity esters are optional. Low to medium-low hop flavor (any variety, but typically floral, spicy, or herbal).",
+				mouthfeel:
+					"Generally light and crisp, although body can reach medium. Smooth mouthfeel with medium to high attenuation; higher attenuation levels can lend a “thirst quenching” quality. High carbonation.",
+				comments:
+					"Pre-prohibition Cream Ales were slightly stronger, hoppier (including some dry hopping) and more bitter (25-30+ IBUs). These versions should be entered in the historical category. Most commercial examples are in the 1.050–1.053 OG range, and bitterness rarely rises above 20 IBUs.",
+				history:
+					"A sparkling or present-use ale that existed in the 1800s and survived prohibition. An ale version of the American lager style. Produced by ale brewers to compete with lager brewers in Canada and the Northeast, Mid-Atlantic, and Midwest states. Originally known as sparkling or present use ales, lager strains were (and sometimes still are) used by some brewers, but were not historically mixed with ale strains. Many examples are kräusened to achieve carbonation. Cold conditioning isn’t traditional, although modern brewers sometimes use it.",
+				characteristic_ingredients:
+					"American ingredients most commonly used. A grain bill of six-row malt, or a combination of six-row and North American two-row, is common. Adjuncts can include up to 20% maize in the mash, and up to 20% glucose or other sugars in the boil. Any variety of hops can be used for bittering and finishing.",
+				style_comparison:
+					"Similar to a Standard American Lager, but with more character.",
 				vital_statistics: {
 					original_extract: { min: 10.4, max: 13.5 },
 					terminal_extract: { min: 1.5, max: 3.1 },
