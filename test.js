c93cb3a661f4bbb2ed244c2d14f4b1d05239d8a7
john muhl <git@johnmuhl.com>
2018-11-11 11:32:24 -0600

    fmt: add JSON

--- test.js
+++ test.js
@@ -12,35 +12,35 @@ const styleValidator = ajv.compile(styleSchema);
 t.assert(Array.isArray(guidelines));
 
 guidelines.forEach(cat => {
-  let valid = categoryValidator(cat);
-  if (!valid) console.error(cat, categoryValidator.errors);
-  t.assert(valid);
+	let valid = categoryValidator(cat);
+	if (!valid) console.error(cat, categoryValidator.errors);
+	t.assert(valid);
 
-  if (cat.styles) {
-    cat.styles.forEach(style => {
-      valid = styleValidator(style);
-      if (!valid) console.error(style, styleValidator.errors);
-      t.assert(valid);
-    });
-  }
+	if (cat.styles) {
+		cat.styles.forEach(style => {
+			valid = styleValidator(style);
+			if (!valid) console.error(style, styleValidator.errors);
+			t.assert(valid);
+		});
+	}
 
-  if (cat.subcategories) {
-    cat.subcategories.forEach(sub => {
-      valid = styleValidator(sub);
-      if (!valid) console.error(sub, styleValidator.errors);
-      t.assert(valid);
+	if (cat.subcategories) {
+		cat.subcategories.forEach(sub => {
+			valid = styleValidator(sub);
+			if (!valid) console.error(sub, styleValidator.errors);
+			t.assert(valid);
 
-      if (sub.styles) {
-        sub.styles.forEach(style => {
-          valid = styleValidator(style);
-          if (!valid) console.error(style, styleValidator.errors);
-          t.assert(valid);
-        });
-      }
-    });
-  }
+			if (sub.styles) {
+				sub.styles.forEach(style => {
+					valid = styleValidator(style);
+					if (!valid) console.error(style, styleValidator.errors);
+					t.assert(valid);
+				});
+			}
+		});
+	}
 });
 
 function loadJSON(path) {
-  return JSON.parse(readFileSync(path, "utf-8"));
+	return JSON.parse(readFileSync(path, "utf-8"));
 }
